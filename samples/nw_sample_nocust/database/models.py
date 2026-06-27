43055a6cf4cf198e00026175a3ba1d1caecd28e8
Val Huber <valjhuber@gmail.com>
2026-06-27 14:23:18 -0700

    Sync from local-mgr (gold v17.01.05)

--- samples/nw_sample_nocust/database/models.py
+++ samples/nw_sample_nocust/database/models.py
@@ -10,7 +10,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # Alter this file per your database maintenance policy
 #    See https://apilogicserver.github.io/Docs/Project-Rebuild/#rebuilding
 #
-# Created:  June 23, 2026 16:31:55
+# Created:  June 27, 2026 14:14:07
 # Database: sqlite:////Users/val/dev/ApiLogicServer/ApiLogicServer-dev/build_and_test/genai-logic/samples/nw_sample_nocust/database/db.sqlite
 # Dialect:  sqlite
 #
